bef0ef395b42fbc55656b8dbf139af842e35e80b
Krishna Heroor <heroorkrishna@gmail.com>
2020-09-28 12:59:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -418,8 +418,8 @@ st.image(image, use_column_width=True)
 def main():
 	st.title("Machine Learning Application for Automated EDA")
 	
-	st.info("This Web Application is created and maintained by *_DHEERAJ_ _KUMAR_ _K_*")
-	"""https://github.com/DheerajKumar97""" 
+	st.info("Web applications created by - Krishna Heoor")
+	"""https://github.com/heroorkrishna""" 
 	activities = ["General EDA","EDA For Linear Models","Model Building for Classification Problem"]	
 	choice = st.sidebar.selectbox("Select Activities",activities)
 
@@ -691,7 +691,8 @@ def main():
 	st.markdown('Automation is **_really_ _cool_**.')
 	st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
 	st.title("Credits and Inspiration")
-	"""https://pycaret.org/"""    
+	"""https://pycaret.org/""" 
+	st.title("Happy Learning")
 
 
 if __name__ == '__main__':
